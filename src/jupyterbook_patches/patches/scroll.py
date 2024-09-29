4bdccaed20ba8dd856090f3ea88d3570a8225ecc
DianaBanta <D.M.Banta-1@student.tudelft.nl>
2024-09-30 01:57:02 +0200

    remove unneeded patches

--- src/jupyterbook_patches/patches/scroll.py
+++
@@ -1,9 +0,0 @@
-from jupyterbook_patches.patches import BasePatch, logger
-
-
-class ScrollPatch(BasePatch):
-    name = "scroll"
-
-    def initialize(self, app):
-        logger.info("Initializing scroll patch")
-        app.add_js_file(filename="scroll.js", loading_method="async")
