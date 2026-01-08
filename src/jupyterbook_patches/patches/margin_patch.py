9e84b1bc9866d5c3cb28d47775fc5fcfda2a9dcf
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-01-08 15:09:50 +0100

    Update margin_patch.py

--- src/jupyterbook_patches/patches/margin_patch.py
+++ src/jupyterbook_patches/patches/margin_patch.py
@@ -5,5 +5,4 @@ class MarginPatch(BasePatch):
     name = "margin"
 
     def initialize(self, app):
-        logger.info("Initializing Margin patch")
         app.add_css_file(filename="margin_patch.css")
