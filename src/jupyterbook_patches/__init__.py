72a7143b9393519969f0ef113af965572eaecd92
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-01-08 15:07:43 +0100

    Remove logger for disabled patches
    Removed logging of disabled patches in init_patches function.

--- src/jupyterbook_patches/__init__.py
+++ src/jupyterbook_patches/__init__.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 
 from sphinx.application import Sphinx
-from sphinx.util import logging
 
 from jupyterbook_patches._version import version as __version__
 from jupyterbook_patches.utils import load_patches
 from jupyterbook_patches.patches.mathjax_patch import set_mathjax_loading
-
-logger = logging.getLogger(__name__)
-
 
 def set_static_path(app):
     static_path = Path(__file__).parent / "patches" / "_static"
@@ -27,11 +23,6 @@ def setup_patch_configuration(app: Sphinx,config):
         raise TypeError(
             "Patch configuration value for 'disabled-patches' must be a list"
         )
-
-    if len(patch_config["disabled-patches"]) == 0:
-        logger.info("All patches enabled")
-    else:
-        logger.info("Disabled patches: %s", ", ".join(patch_config["disabled-patches"]))
 
 
 def init_patches(app: Sphinx,config):
