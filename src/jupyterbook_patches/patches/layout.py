0fab905e5e07695bb0aea97d181cd1c417002a27
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-06-19 10:32:46 +0200

    Rename CSS file from fix-todo.css to fix_todo.css

--- src/jupyterbook_patches/patches/layout.py
+++ src/jupyterbook_patches/patches/layout.py
@@ -12,4 +12,4 @@ class DarkModePatch(BasePatch):
         app.add_css_file(filename="fix_margin.css")
         app.add_css_file(filename="fix_align_text_captions.css")
         app.add_css_file(filename="fix_align_code.css")
-        app.add_css_file(filename="fix-todo.css")
+        app.add_css_file(filename="fix_todo.css")
